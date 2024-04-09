b8f50a448b2073be53dc538a9c6f7cd167fba635
tdl-duke <166145483+tdl-duke@users.noreply.github.com>
2024-04-09 18:45:08 -0400

    Working VGA Code
    All necessary updated files to get VGA to display a mem file

---
+++ Wrapper.v
@@ -0,0 +1,169 @@
+`timescale 1ns / 1ps
+/**
+ * 
+ * READ THIS DESCRIPTION:
+ *
+ * This is the Wrapper module that will serve as the header file combining your processor, 
+ * RegFile and Memory elements together.
+ *
+ * This file will be used to generate the bitstream to upload to the FPGA.
+ * We have provided a sibling file, Wrapper_tb.v so that you can test your processor's functionality.
+ * 
+ * We will be using our own separate Wrapper_tb.v to test your code. You are allowed to make changes to the Wrapper files 
+ * for your own individual testing, but we expect your final processor.v and memory modules to work with the 
+ * provided Wrapper interface.
+ * 
+ * Refer to Lab 5 documents for detailed instructions on how to interface 
+ * with the memory elements. Each imem and dmem modules will take 12-bit 
+ * addresses and will allow for storing of 32-bit values at each address. 
+ * Each memory module should receive a single clock. At which edges, is 
+ * purely a design choice (and thereby up to you). 
+ * 
+ * You must change line 36 to add the memory file of the test you created using the assembler
+ * For example, you would add sample inside of the quotes on line 38 after assembling sample.s
+ *
+ **/
+
+module Wrapper (CLK100MHZ, BTNC, LED, hSync, vSync, VGA_B, VGA_G, VGA_R);
+	input CLK100MHZ, BTNC;
+	output[7:0] LED;
+	output vSync; 		// H Sync Signal
+	output hSync; 		// Veritcal Sync Signal
+	output[3:0] VGA_R;  // Red Signal Bits
+	output[3:0] VGA_G;  // Green Signal Bits
+	output[3:0] VGA_B;  // Blue Signal Bits
+
+	wire rwe, mwe, clock, reset;
+	wire[4:0] rd, rs1, rs2;
+	wire[31:0] instAddr, instData, 
+		rData, regA, regB,
+		memAddr, memDataIn, memDataOut, pc;
+		
+	assign clock = CLK100MHZ;
+	assign reset = BTNC;
+    
+    localparam FILES_PATH = "C:/Users/tdl21/Desktop/";
+    
+	wire active, screenEnd;
+	wire[9:0] x;
+	wire[8:0] y;
+	wire clk25; // 25MHz clock
+
+	reg[1:0] pixCounter = 0;      // Pixel counter to divide the clock
+    assign clk25 = pixCounter[1]; // Set the clock high whenever the second bit (2) is high
+	always @(posedge clock) begin
+		pixCounter <= pixCounter + 1; // Since the reg is only 3 bits, it will reset every 8 cycles
+	end
+	
+	//VGA
+    localparam 
+		VIDEO_WIDTH = 640,  // Standard VGA Width
+		VIDEO_HEIGHT = 480; // Standard VGA Height
+    
+    VGATimingGenerator #(
+		.HEIGHT(VIDEO_HEIGHT), // Use the standard VGA Values
+		.WIDTH(VIDEO_WIDTH))
+	Display( 
+		.clk25(clk25),  	   // 25MHz Pixel Clock
+		.reset(reset),		   // Reset Signal
+		.screenEnd(screenEnd), // High for one cycle when between two frames
+		.active(active),	   // High when drawing pixels
+		.hSync(hSync),  	   // Set Generated H Signal
+		.vSync(vSync),		   // Set Generated V Signal
+		.x(x), 				   // X Coordinate (from left)
+		.y(y)); 			   // Y Coordinate (from top)	   
+
+// Image Data to Map Pixel Location to Color Address
+	localparam 
+		PIXEL_COUNT = VIDEO_WIDTH*VIDEO_HEIGHT, 	             // Number of pixels on the screen
+		PIXEL_ADDRESS_WIDTH = $clog2(PIXEL_COUNT) + 1,           // Use built in log2 command
+		BITS_PER_COLOR = 12, 	  								 // Nexys A7 uses 12 bits/color
+		PALETTE_COLOR_COUNT = 256, 								 // Number of Colors available
+		PALETTE_ADDRESS_WIDTH = $clog2(PALETTE_COLOR_COUNT) + 1; // Use built in log2 Command
+
+	wire[PIXEL_ADDRESS_WIDTH-1:0] imgAddress;  	 // Image address for the image data
+	wire[PALETTE_ADDRESS_WIDTH-1:0] colorAddr; 	 // Color address for the color palette
+	assign imgAddress = x + 640*y;				 // Address calculated coordinate
+
+	IRAM #(		
+		.DEPTH(PIXEL_COUNT), 				     // Set RAM depth to contain every pixel
+		.DATA_WIDTH(PALETTE_ADDRESS_WIDTH),      // Set data width according to the color palette
+		.ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH),     // Set address with according to the pixel count
+		.MEMFILE({FILES_PATH, "image.mem"})) // Memory initialization
+	ImageData(
+		.clk(clock), 						 // Falling edge of the 100 MHz clk
+		.addr(imgAddress),					 // Image data address
+		.dataOut(colorAddr),				 // Color palette address
+		.wEn(1'b0)); 						 // We're always reading
+
+	// Color Palette to Map Color Address to 12-Bit Color
+	wire[BITS_PER_COLOR-1:0] colorData; // 12-bit color data at current pixel
+
+	IRAM #(
+		.DEPTH(PALETTE_COLOR_COUNT), 		       // Set depth to contain every color		
+		.DATA_WIDTH(BITS_PER_COLOR), 		       // Set data width according to the bits per color
+		.ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH),     // Set address width according to the color count
+		.MEMFILE({FILES_PATH, "colors.mem"}))  // Memory initialization
+	ColorPalette(
+		.clk(clock), 							   	   // Rising edge of the 100 MHz clk
+		.addr(colorAddr),					       // Address from the ImageData RAM
+		.dataOut(colorData),				       // Color at current pixel
+		.wEn(1'b0)); 						       // We're always reading
+	
+
+	// Assign to output color from register if active
+	wire[BITS_PER_COLOR-1:0] colorOut; 			  // Output color 
+	assign colorOut = active ? colorData : 12'd0; // When not active, output black
+
+	// Quickly assign the output colors to their channels using concatenation
+	assign {VGA_R, VGA_G, VGA_B} = colorOut;
+    //assign LED[0] = vSync;
+    //assign LED[1] = hSync;
+    //assign LED[2] = (VGA_R != 4'b0);
+    //assign LED[3] = (VGA_B != 4'b0);
+    //assign LED[4] = (VGA_G != 4'b0);
+    
+    //assign LED = pc[7:0];
+    //assign LED = 8'b10101010;
+
+	// ADD YOUR MEMORY FILE HERE
+	localparam INSTR_FILE = "";
+	
+	
+	// Main Processing Unit
+	processor CPU(.clock(clock), .reset(reset), 
+								
+		// ROM
+		.address_imem(instAddr), .q_imem(instData),
+									
+		// Regfile
+		.ctrl_writeEnable(rwe),     .ctrl_writeReg(rd),
+		.ctrl_readRegA(rs1),     .ctrl_readRegB(rs2), 
+		.data_writeReg(rData), .data_readRegA(regA), .data_readRegB(regB),
+									
+		// RAM
+		.wren(mwe), .address_dmem(memAddr), 
+		.data(memDataIn), .q_dmem(memDataOut),
+		.out_pc(pc)); 
+	
+	// Instruction Memory (ROM)
+	ROM #(.MEMFILE({INSTR_FILE, ".mem"}))
+	InstMem(.clk(clock), 
+		.addr(instAddr[11:0]), 
+		.dataOut(instData));
+	
+	// Register File
+	regfile RegisterFile(.clock(clock), 
+		.ctrl_writeEnable(rwe), .ctrl_reset(reset), 
+		.ctrl_writeReg(rd),
+		.ctrl_readRegA(rs1), .ctrl_readRegB(rs2), 
+		.data_writeReg(rData), .data_readRegA(regA), .data_readRegB(regB));
+						
+	// Processor Memory (RAM)
+	RAM ProcMem(.clk(clock), 
+		.wEn(mwe), 
+		.addr(memAddr[11:0]), 
+		.dataIn(memDataIn), 
+		.dataOut(memDataOut));
+
+endmodule
